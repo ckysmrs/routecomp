4469e9e20da7f5e1678123d781b9bdd26d024ac1
ckysmrs <ckysmrs@gmail.com>
2024-06-20 23:50:22 +0900

    BinaryHeapを追加

---
+++ binary_heap.py
@@ -0,0 +1,92 @@
+# This file is based on
+# https://github.com/dilsonpereira/Minimum-Cost-Perfect-Matching
+
+# This is a binary heap for pairs of the type (Decimal key, int satellite)
+# It is assumed that satellites are unique integers
+# This is the case with graph algorithms, in which satellites are vertex or edge indices
+
+from decimal import Decimal
+
+class BinaryHeap:
+    def __init__(self):
+        self.key: dict[int, Decimal] = {}  # Given the satellite, this is its key
+        self.pos: dict[int, int] = {}  # Given the satellite, this is its position in the heap
+        self.satellite: list[int] = [0]  # This is the heap!
+        self.size: int = 0  # Number of elements in the heap
+    
+    # Inserts (key k, satellite s) in the heap
+    def insert(self, k: Decimal, s: int) -> None:
+        # If satellite is already in the heap
+        if s in self.pos:
+            raise ValueError('Error: satellite already in heap')
+    
+        self.satellite.append(0)
+        self.size += 1
+        i = self.size
+        while i // 2 > 0 and self.key[self.satellite[i // 2]] > k:
+            self.satellite[i] = self.satellite[i // 2]
+            self.pos[self.satellite[i]] = i
+            i //= 2
+        self.satellite[i] = s
+        self.pos[s] = i
+        self.key[s] = k
+    
+    # Deletes the element with minimum key and returns its satellite information
+    def delete_min(self) -> int:
+        if self.size == 0:
+            raise IndexError('Error: empty heap')
+    
+        min = self.satellite[1]
+        slast = self.satellite[self.size]
+        self.size -= 1
+    
+        child = 2
+        i = 1
+        while child  <= self.size:
+            if child < self.size and self.key[self.satellite[child]] > self.key[self.satellite[child + 1]]:
+                child += 1
+    
+            if self.key[slast] > self.key[self.satellite[child]]:
+                self.satellite[i] = self.satellite[child]
+                self.pos[self.satellite[child]] = i
+            else:
+                break
+            i = child
+            child *= 2
+        self.satellite[i] = slast
+        self.pos[slast] = i
+    
+        del self.key[min]
+        del self.pos[min]
+        del self.satellite[-1]
+
+        return min
+    
+    # Changes the key of the element with satellite s
+    def change_key(self, k: Decimal, s: int) -> None:
+        self.remove(s)
+        self.insert(k, s)
+    
+    # Removes the element with satellite s
+    def remove(self, s: int) -> None:
+        i = self.pos[s]
+        while i // 2 > 0:
+            self.satellite[i] = self.satellite[i // 2]
+            self.pos[self.satellite[i]] = i
+            i //= 2
+        self.satellite[1] = s
+        self.pos[s] = 1
+    
+        self.delete_min()
+
+    # Returns the number of elements in the heap
+    def __len__(self) -> int:
+        return self.size
+    
+    # Resets the structure
+    def clear(self) -> None:
+        self.key.clear()
+        self.pos.clear()
+        self.satellite.clear()
+        self.satellite.append(0)
+        self.size = 0
---
+++ testsrc/binary_heap_test.py
@@ -0,0 +1,83 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import unittest
+from decimal import Decimal
+from binary_heap import BinaryHeap
+
+class BinaryHeapTest(unittest.TestCase):
+    def test_get_min(self):
+        # 最小値のサテライトを返す
+        satelittes = (4, 2, 6, 3, 0)
+        keys = (Decimal('0.7'), Decimal('0.2'), Decimal('0.4'), Decimal('0.8'), Decimal('0.3'))
+        sut = BinaryHeap()
+        for k, s in zip(keys, satelittes):
+            sut.insert(k, s)
+        self.assertEqual(sut.delete_min(), 2)
+        self.assertEqual(sut.delete_min(), 0)
+        self.assertEqual(sut.delete_min(), 6)
+        self.assertEqual(sut.delete_min(), 4)
+        self.assertEqual(sut.delete_min(), 3)
+
+    def test_same_satelette(self):
+        # 同じサテライトに登録すると例外を出す
+        sut = BinaryHeap()
+        sut.insert(Decimal('1'), 0)
+        with self.assertRaises(ValueError, msg='Error: satellite already in heap'):
+            sut.insert(Decimal('2'), 0)
+
+    def test_len(self):
+        # サイズを返す
+        sut = BinaryHeap()
+        self.assertEqual(len(sut), 0)
+        sut.insert(Decimal('1'), 0)
+        self.assertEqual(len(sut), 1)
+        sut.delete_min()
+        self.assertEqual(len(sut), 0)
+
+    def test_no_data(self):
+        # データが無いのに取り出そうとすると例外を出す
+        sut = BinaryHeap()
+        with self.assertRaises(IndexError, msg='Error: empty heap'):
+            sut.delete_min()
+
+    def test_change_key(self):
+        # キーを変える
+        sut = BinaryHeap()
+        sut.insert(Decimal('0.1'), 0)
+        sut.insert(Decimal('0.2'), 1)
+        sut.change_key(Decimal('0.3'), 0)
+        self.assertEqual(sut.delete_min(), 1)
+
+    def test_remove(self):
+        # サテライト指定でキーを削除
+        satelittes = (4, 2, 6, 3, 0)
+        keys = (Decimal('0.7'), Decimal('0.2'), Decimal('0.4'), Decimal('0.8'), Decimal('0.3'))
+        sut = BinaryHeap()
+        for k, s in zip(keys, satelittes):
+            sut.insert(k, s)
+        sut.remove(6)
+        self.assertEqual(sut.delete_min(), 2)
+        self.assertEqual(sut.delete_min(), 0)
+        self.assertEqual(sut.delete_min(), 4)
+        self.assertEqual(sut.delete_min(), 3)
+
+    def test_clear(self):
+        # データのクリア
+        satelittes = (4, 2, 6, 3, 0)
+        keys = (Decimal('0.7'), Decimal('0.2'), Decimal('0.4'), Decimal('0.8'), Decimal('0.3'))
+        sut = BinaryHeap()
+        for k, s in zip(keys, satelittes):
+            sut.insert(k, s)
+        self.assertEqual(len(sut), 5)
+        sut.clear()
+        self.assertEqual(len(sut), 0)
+        for k, s in zip(keys, satelittes):
+            sut.insert(k, s)
+        self.assertEqual(len(sut), 5)
+        self.assertEqual(sut.delete_min(), 2)
+        self.assertEqual(sut.delete_min(), 0)
+        self.assertEqual(sut.delete_min(), 6)
+        self.assertEqual(sut.delete_min(), 4)
+        self.assertEqual(sut.delete_min(), 3)
